090ec4ad47ed8ebe68f63ad21a44d6e36b0fb6ff
Matthew Struble <matt@vevox.io>
2016-11-24 14:22:59 -0800

    Added some options to the window

--- src/index.js
+++ src/index.js
@@ -9,7 +9,12 @@ exports.init = a => {
 
   let win = new electron.BrowserWindow({
     width: 1024,
-    height: 600
+    height: 600,
+    frame: false,
+    resizable: false,
+    fullscreenable: false,
+    maximizeable: false,
+    title: 'Taking Inventory' // TODO Set window title with version
   });
 
   win.loadURL(`file://${app.__rootdir}/index.html`);
