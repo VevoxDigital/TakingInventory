6622cbc271eee9dfeab16a7abfd070ca7b6fd574
Matthew Struble <matt@vevox.io>
2016-11-25 22:08:40 -0800

    Moved events to own file

---
+++ public/js/events.js
@@ -0,0 +1,28 @@
+'use strict';
+
+exports = class EventEmitter {
+  constructor() {
+    this.listeners = new Map();
+  }
+
+  on(label, cb) {
+    if (!this.listeners.has(label))
+      this.listeners.set(label, { on: [], once: [] });
+    this.listeners.get(label).on.push(cb);
+  }
+
+  once(label, cb) {
+    if (!this.listeners.has(label))
+      this.listeners.set(label, { on: [], once: [] });
+    this.listeners.get(label).once.push(cb);
+  }
+
+  emit(label) {
+    let callbacks = this.listeners.get(label) || { on: [], once: [] };
+    callbacks.on.forEach(cb => cb.apply(null, Array.prototype.slice.call(arguments, 1)));
+    callbacks.once.forEach(cb => cb.apply(null, Array.prototype.slice.call(arguments, 1)));
+    callbacks.once = [];
+    this.listeners.set(label, callbacks);
+  }
+
+}
--- public/js/launcher.js
+++ public/js/launcher.js
@@ -3,32 +3,7 @@
 const {remote} = require('electron');
 window.$ = window.jQuery = require('jquery');
 
-class EventEmitter {
-  constructor() {
-    this.listeners = new Map();
-  }
-
-  on(label, cb) {
-    if (!this.listeners.has(label))
-      this.listeners.set(label, { on: [], once: [] });
-    this.listeners.get(label).on.push(cb);
-  }
-
-  once(label, cb) {
-    if (!this.listeners.has(label))
-      this.listeners.set(label, { on: [], once: [] });
-    this.listeners.get(label).once.push(cb);
-  }
-
-  emit(label) {
-    let callbacks = this.listeners.get(label) || { on: [], once: [] };
-    callbacks.on.forEach(cb => cb.apply(null, Array.prototype.slice.call(arguments, 1)));
-    callbacks.once.forEach(cb => cb.apply(null, Array.prototype.slice.call(arguments, 1)));
-    callbacks.once = [];
-    this.listeners.set(label, callbacks);
-  }
-
-}
+const EventEmitter = require('./events');
 
 class Launcher extends EventEmitter {
 
