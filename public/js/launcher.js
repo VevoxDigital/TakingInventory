5a75a58cb0d2822852d39a7b92aa1af1954f2476
Matthew Struble <matt@vevox.io>
2016-11-25 21:36:26 -0800

    More work on the launch button

--- public/js/launcher.js
+++ public/js/launcher.js
@@ -3,6 +3,53 @@
 const {remote} = require('electron');
 window.$ = window.jQuery = require('jquery');
 
+class EventEmitter {
+  constructor() {
+    this.listeners = new Map();
+  }
+
+  on(label, cb) {
+    if (!this.listeners.has(label))
+      this.listeners.set(label, { on: [], once: [] });
+    this.listeners.get(label).on.push(cb);
+  }
+
+  once(label, cb) {
+    if (!this.listeners.has(label))
+      this.listeners.set(label, { on: [], once: [] });
+    this.listeners.get(label).once.push(cb);
+  }
+
+  emit(label) {
+    let callbacks = this.listeners.get(label) || { on: [], once: [] };
+    callbacks.on.forEach(cb => cb.apply(null, Array.prototype.slice.call(arguments, 1)));
+    callbacks.once.forEach(cb => cb.apply(null, Array.prototype.slice.call(arguments, 1)));
+    callbacks.once = [];
+    this.listeners.set(label, callbacks);
+  }
+
+}
+
+class Launcher extends EventEmitter {
+
+  launch() {
+    this.emit('gameLaunched');
+  }
+
+  get activeProfile() {
+    return this.$activeProfile;
+  }
+
+  set activeProfile(profile) {
+    if (profile) this.$activeProfile = profile;
+    else delete this.$activeProfile;
+    this.emit('profileChanged', this.$activeProfile);
+  }
+
+}
+
+let launcher;
+
 $(() => {
 
   $('#windowClose').click(() => {
@@ -12,8 +59,32 @@ $(() => {
     remote.getCurrentWindow().minimize();
   });
 
-  setTimeout(() => {
-    $('#launch').removeAttr('disabled');
-  }, 1000);
+  function updateLaunchButton(profile) {
+    let button = $('#launch'),
+      primary = button.find('.primary'),
+      secondary = button.find('.secondary');
+    if (profile) {
+      primary.html('Launch Minecraft');
+      secondary.html(profile.name + ' - ' + profile.version);
+      button.attr('title', 'Ready to Launch Minecraft ' + profile.version);
+      button.removeAttr('disabled');
+    } else {
+      primary.html('Please Wait');
+      secondary.html('Loading profile manifest, just a moment...');
+      button.attr('title', 'Loading...');
+      button.attr('disabled', '');
+    }
+  }
+  updateLaunchButton();
+
+  launcher = new Launcher();
+  launcher.on('profileChanged', profile => {
+    updateLaunchButton(profile);
+  });
+
+  // TODO DEBUG Just tied the menu button to a mock profile for testing.
+  $('#windowOptions').click(() => {
+    launcher.activeProfile = launcher.activeProfile ? null : { name: 'Latest Official', version: '1.10.2' };
+  });
 
 });
