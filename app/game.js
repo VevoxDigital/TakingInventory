a171e794575264d01e4d8095a191a6da732c0fe6
Matthew Struble <matt@vevox.io>
2016-11-29 22:39:32 -0800

    Added some launcher config stuff

---
+++ app/game.js
@@ -0,0 +1,367 @@
+'use strict';
+
+const path  = require('path'),
+      q     = require('q'),
+      os    = require('os'),
+      fs    = require('fs-extra'),
+      https = require('https');
+
+const {app} = require('electron');
+
+const DIR_VERSIONS = 'versions';
+
+const FILE_VERSIONS = 'version_manifest.json',
+      FILE_PROFILES = 'profiles.json';
+
+
+const LAUNCHER_PROFILE_FORMAT = 1;
+exports.LauncherVisibility = {
+  STAY_HIDDEN: 0,
+  STAY_VISIBLE: 1,
+  HIDE_THEN_SHOW: 2
+};
+
+exports.Profile = class Profile {
+  static deserialize(input) {
+    return new this(JSON.parse(input));
+  }
+
+  constructor(data) {
+    if (typeof data === 'string') {
+      this.name = data;
+    } else if (typeof json === 'object') {
+      if (!json.name) throw new Error('name property missing');
+      this.name = json.name;
+      this.version = json.version;
+      this.java = json.java;
+      this.resolution = json.resolution;
+      this.visibility = json.visibility;
+      this.game = json.game;
+    } else throw new Error('unknown constructor data');
+  }
+
+  serialize() {
+    return JSON.stringify({
+      name: this.name,
+      version: this.version,
+      java: this.java,
+      resolution: this.resolution,
+      visiblity: this.visiblity,
+      game: this.game
+    })
+  }
+};
+
+exports.User = class User {
+  constructor(json) {
+    this.name = json.name;
+    this.username = json.username;
+    this.id = json.id;
+    this.token = json.token;
+    this.uuid = json.uuid;
+  }
+};
+exports.LauncherConfig = class LauncherConfig {
+  static deserialize(json) {
+    return new this(JSON.parse(json));
+  }
+
+  static load() {
+    let deferred = q.defer(),
+        self = this;
+
+    // try to load the profiles file
+    let dir = exports.dir();
+    fs.readFile(path.join(dir, FILE_PROFILES), (err, data) => {
+      if (err) {
+        app.logger.info('no launcher config found, probably a clean install');
+        // TODO look for Mojang launcher config and import
+        deferred.resolve(new self('Latest Official'));
+      } else deferred.resolve(self.deserialize(data.toString()));
+    });
+
+    return deferred.promise;
+  }
+
+  constructor(data) {
+    this.profiles = { };
+    if (typeof data === 'object') {
+      let ps = data.profiles || { };
+      for (const p in ps) if (ps.hasOwnProperty(p))
+        this.profiles[p] = new exports.Profile(ps[p]);
+      this.profile = data.profile;
+    } else if (typeof data === 'string') {
+      this.profiles[data] = new exports.Profile(data);
+    } else throw new Error('unknown data input');
+  }
+
+  get format() {
+    return LAUNCHER_PROFILE_FORMAT;
+  }
+
+  set profile(profile) {
+    if (!this.profiles[profile]) return;
+    this.$profile = profile;
+  }
+  get profile() {
+    return this.profiles[this.$profile];
+  }
+
+  get numProfiles() {
+    return Object.keys(this.profiles).length;
+  }
+
+  serialize() {
+    let ps = { };
+    for (const p in this.profiles) if (this.profiles.hasOwnProperty(p))
+      ps[p] = this.profiles[p].serialize();
+    return JSON.stringify({
+      profiles: ps,
+      profile: this.profile,
+      format: this.format
+    });
+  }
+
+  save() {
+    let deferred = q.defer(),
+        self = this;
+
+    fs.writeFile(path.join(exports.dir(), FILE_PROFILES), self.serialize(), err => {
+      if (err) return deferred.reject(err);
+      deferred.resolve();
+    });
+
+    return deferred.promise;
+  }
+};
+
+
+exports.VersionType = {
+  RELEASE:  'release',
+  SNAPSHOT: 'snapshot',
+  ALPHA:    'alpha',
+  BETA:     'beta',
+  CUSTOM:   'custom'
+};
+exports.VersionEntry = class VersionEntry {
+  constructor(id = 'undefined', type = exports.VersionType.CUSTOM, url) {
+    this.id = id;
+    this.type = type;
+    this.url = url;
+  }
+
+  /**
+    * Determines if this VersionEntry has a downloadable jar
+    * (i.e. entry.url is truthy)
+    * @returns True if downloadable, false otherwise.
+    */
+  downloadable() {
+    return !!this.url;
+  }
+
+  /**
+    * Determines if this VersionEntry is of the given version type.
+    * @returns True if version types match, false otherwise
+    */
+  is(versionType) {
+    return this.type === versionType;
+  }
+
+  /**
+    * Serializes this VersionEntry to json
+    * @returns JSON
+    */
+  json() {
+    return {
+      id: this.id,
+      type: this.type,
+      url: this.url
+    };
+  }
+
+  // disable setters, as all are constant
+  set id(id) { }
+  set type(type) { }
+  set url(url) { }
+
+};
+
+
+/**
+  * Returns an absolute path of the game directory on the given paltform.
+  * If no platform is given, `os.platform()` is used.
+  * @name game.dir
+  * @param platform The OS platform.
+  * @returns The game directory
+  * @since 1.0.0
+  */
+exports.dir = platform => {
+  switch (platform || os.platform()) {
+    case 'darwin':
+      return path.join(os.homedir(), 'Library', 'Application Support', 'minecraft');
+    case 'win32':
+      return path.join(os.homedir(), 'AppData', 'Roaming', '.minecraft');
+    default:
+      return path.join(os.homedir(), '.minecraft');
+  }
+}
+
+/**
+  * Downloads the current version manifest from Mojang, deleting
+  * the existing manifest on disk.
+  * @name game.downloadVersions
+  * @returns The versions manifest that was downloaded.
+  * @since 1.0.0
+  */
+exports.downloadVersions = versionsManifestFile => {
+  let deferred = q.defer(),
+      self = this;
+
+  // remove the pre-existing file so the stream doesn't fail
+  try {
+    fs.removeSync(versionsManifestFile);
+    app.logger.verbose('successfully removed pre-existing file');
+  } catch (err) {
+    app.logger.verbose(err.toString());
+  }
+
+  // create a write stream and make the request
+  let versionsManifestFileStream = fs.createWriteStream(versionsManifestFile);
+  let req = https.request({
+    host: 'launchermeta.mojang.com',
+    path: '/mc/game/version_manifest.json',
+    timeout: 5000
+  }, res => {
+    let contents = '';
+    app.logger.verbose('version manifest GET ' + res.statusCode);
+    versionsManifestFileStream.on('error', err => {
+      deferred.reject(err);
+    });
+
+    res.on('data', data => {
+      contents += data.toString();
+      versionsManifestFileStream.write(data);
+    });
+
+    res.on('end', () => {
+      app.logger.info('downloaded manifest: length ' + contents.length);
+      versionsManifestFileStream.end();
+      versionsManifestFileStream.close();
+      deferred.resolve(JSON.parse(contents));
+    });
+  });
+  req.on('error', err => {
+    console.log(err);
+    deferred.reject(err);
+  });
+  req.end();
+
+  return deferred.promise;
+};
+
+/**
+  * Fetches the contents of the offical version manifest on disk, downloading
+  * the manifest if it differs from the server or is missing.
+  * @name game.versionsOffical
+  * @returns The offical versions
+  * @since 1.0.0
+  */
+exports.versionsOffical = () => {
+  let deferred = q.defer();
+
+  // make sure the versions directory exists
+  const versionsDir = path.join(exports.dir(), DIR_VERSIONS);
+  fs.ensureDir(versionsDir, err => {
+    if (err) return deferred.reject(err);
+
+    // see if the version manifest exists
+    let versionsManifestFile = path.join(versionsDir, FILE_VERSIONS);
+    fs.stat(versionsManifestFile, (err, stats) => {
+      if (err) {
+        // manifest is missing, download it.
+        app.logger.info('version manifest is missing and will download');
+        deferred.resolve(exports.downloadVersionManifest(versionsManifestFile));
+      } else {
+        // existing manifest found, see if it needs updating
+        app.logger.info('checking for updates...');
+        fs.readFile(versionsManifestFile, (err, contents) => {
+          if (err) return deferred.reject(err);
+
+          // make a HEAD request to check 'content-length'
+          let req = https.request({
+            host: 'launchermeta.mojang.com',
+            path: '/mc/game/version_manifest.json',
+            method: 'HEAD'
+          }, res => {
+            let targetLength = parseInt(res.headers['content-length'], 10),
+                localLength = contents.length;
+            app.logger.verbose(`target ${targetLength} vs source ${localLength}`);
+
+            // Redownload is length differs, otherwise return the contents
+            if (targetLength === localLength) {
+              app.logger.info('no new updates, keeping local version');
+              deferred.resolve(JSON.parse(contents));
+            } else {
+              app.logger.info('updates are available, downloading latest manifest...');
+              setTimeout(() => {
+                // timeout to avoid throttling
+                deferred.resolve(exports.downloadVersionManifest(versionsManifestFile));
+              }, 1000);
+            }
+          });
+          req.on('error', err => {
+            deferred.reject(err);
+          });
+          req.end();
+        });
+      }
+    });
+  });
+
+  return deferred.promise;
+};
+
+/**
+  * Gets a list of all versions available to the client, including unofficial
+  * @returns All available versions
+  * @since 1.0.0
+  */
+exports.versions = () => {
+  return exports.versionsOffical().then((official) => {
+    let deferred = q.defer();
+
+    let versions = { latest: official.latest, versions: { } };
+    official.versions.forEach(version => {
+      versions.versions[version.id] = new exports.VersionEntry(version.id, version.type, version.url);
+    });
+
+    // look through versions dir for non-official versions
+    fs.readdir(path.join(exports.dir(), DIR_VERSIONS), (err, files) => {
+      if (err) return deferred.reject(err);
+
+      for (const id of files) {
+        try {
+          let file = path.join(exports.getGameDirectory(), 'versions', file),
+              stats = fs.statSync(file);
+          // ignore non-directories and existing versions
+          if (stats.isDirectory() && !versions.versions[id]) {
+            try {
+              fs.statSync(path.join(file, id + '.json'));
+              app.logger.verbose('found valid custom version: ' + id);
+              versions.versions[id] = new exports.VersionEntry(id, exports.VersionType.CUSTOM);
+            } catch (e) { continue; }
+          }
+        } catch (e) {
+          app.logger.verbose('failed to read version directory: ' + e.message);
+          continue;
+        }
+      }
+      app.logger.info(Object.keys(versions.versions).length + ' versions found (' +
+        (Object.keys(versions.versions).length - official.versions.length) + ' custom)');
+
+      deferred.resolve(versions);
+    });
+
+    return deferred.promise;
+  });
+};
--- app/windows/launcher.js
+++ app/windows/launcher.js
@@ -2,11 +2,10 @@
 
 const {app, BrowserWindow}  = require('electron');
 
-const path  = require('path'),
-      q     = require('q'),
-      os    = require('os'),
-      fs    = require('fs-extra'),
-      https = require('https');
+const q     = require('q'),
+      path  = require('path');
+
+const game = require('../game');
 
 exports = module.exports = class LauncherWindow extends BrowserWindow {
 
@@ -28,8 +27,8 @@ exports = module.exports = class LauncherWindow extends BrowserWindow {
     self.loadURL('file://' + path.join(app.cwd, 'launcher.html'));
 
     self.once('ready-to-show', () => {
-      this.buildManifests().then(() => {
-        app.logger.info('launcher init complete');
+      this.buildConfig().then(config => {
+        app.logger.info(`launcher ready, loaded ${config.numProfiles} profiles [format ${config.format}]`);
         this.show();
       }).fail((err) => {
         app.logger.error('the launcher could not be initialized');
@@ -41,171 +40,17 @@ exports = module.exports = class LauncherWindow extends BrowserWindow {
     self.logger = app.logger;
   }
 
-  getGameDirectory() {
-    switch (os.platform()) {
-      case 'darwin':
-        return path.join(os.homedir(), 'Library', 'Application Support', 'minecraft');
-      case 'win32':
-        return path.join(os.homedir(), 'AppData', 'Roaming', '.minecraft');
-      default:
-        return path.join(os.homedir(), '.minecraft');
-    }
-  }
+  buildConfig () {
+    return game.LauncherConfig.load().then(config => {
+      let deferred = q.defer();
 
-  downloadVersionManifest(versionsManifestFile) {
-    let deferred = q.defer(),
-        self = this;
-
-    try {
-      fs.removeSync(versionsManifestFile);
-      app.logger.verbose('successfully removed pre-existing file');
-    } catch (err) {
-      app.logger.verbose(err.toString());
-    }
-
-    let versionsManifestFileStream = fs.createWriteStream(versionsManifestFile);
-    let req = https.request({
-      host: 'launchermeta.mojang.com',
-      path: '/mc/game/version_manifest.json',
-      timeout: 5000
-    }, res => {
-      let contents = '';
-      app.logger.verbose('version manifest GET ' + res.statusCode);
-      versionsManifestFileStream.on('error', err => {
-        deferred.reject(err);
-      });
-
-      res.on('data', data => {
-        contents += data.toString();
-        versionsManifestFileStream.write(data);
-      });
-
-      res.on('end', () => {
-        app.logger.info('downloaded manifest: length ' + contents.length);
-        versionsManifestFileStream.end();
-        versionsManifestFileStream.close();
-        deferred.resolve(JSON.parse(contents));
-      });
-    });
-    req.on('error', err => {
-      console.log(err);
-      deferred.reject(err);
-    });
-    req.end();
-
-    return deferred.promise;
-  }
-
-  fetchOfficialVersionManifest() {
-    let deferred = q.defer(),
-        self = this;
-
-    const gameDir = self.getGameDirectory();
-
-    const versionsDir = path.join(gameDir, 'versions');
-    fs.ensureDir(versionsDir, err => {
-      if (err) return deferred.reject(err);
-
-      let versionsManifestFile = path.join(versionsDir, 'version_manifest.json');
-      fs.stat(versionsManifestFile, (err, stats) => {
-        if (err) {
-          app.logger.info('version manifest is missing and will download');
-          deferred.resolve(self.downloadVersionManifest(versionsManifestFile));
-        } else {
-          app.logger.info('checking for updates...');
-          fs.readFile(versionsManifestFile, (err, contents) => {
-            if (err) return deferred.reject(err);
-
-            let req = https.request({
-              host: 'launchermeta.mojang.com',
-              path: '/mc/game/version_manifest.json',
-              method: 'HEAD'
-            }, res => {
-              let targetLength = parseInt(res.headers['content-length'], 10),
-                  localLength = contents.length;
-              app.logger.verbose(`target ${targetLength} vs source ${localLength}`);
-              if (targetLength === localLength) {
-                app.logger.info('no new updates, keeping local version');
-                deferred.resolve(JSON.parse(contents));
-              } else {
-                app.logger.info('updates are available, downloading latest manifest...');
-                setTimeout(() => {
-                  deferred.resolve(self.downloadVersionManifest(versionsManifestFile));
-                }, 1000);
-              }
-            });
-            req.on('error', err => {
-              deferred.reject(err);
-            });
-            req.end();
-          });
-        }
-      });
-    });
-
-    return deferred.promise;
-  }
-
-  buildVersionManifest() {
-    return this.fetchOfficialVersionManifest().then((official) => {
-      let deferred = q.defer(),
-          self = this;
-
-      let versions = { latest: official.latest, versions: { } };
-      official.versions.forEach(version => {
-        versions.versions[version.id] = version;
-      });
-
-      fs.readdir(path.join(self.getGameDirectory(), 'versions'), (err, files) => {
-        if (err) return deferred.reject(err);
-
-        for (const id of files) {
-          try {
-            let file = path.join(self.getGameDirectory(), 'versions', file),
-                stats = fs.statSync(file);
-            // ignore files, as versions are always in directories
-            if (stats.isDirectory() && !versions.versions[id]) {
-              try {
-                fs.statSync(path.join(file, id + '.json'));
-                app.logger.verbose('found valid custom version: ' + id);
-                versions.versions[id] = {
-                  id: id,
-                  type: 'custom'
-                };
-              } catch (e) {
-                continue;
-              }
-            }
-          } catch (e) {
-            app.logger.verbose('failed to read version directory: ' + e.message);
-            continue;
-          }
-        }
-        app.logger.info(Object.keys(versions.versions).length + ' versions found (' +
-          (Object.keys(versions.versions).length - official.versions.length) + ' custom)');
-
-        deferred.resolve(versions);
+      game.versions().then(versions => {
+        config.versions = versions;
+        deferred.resolve(config);
       });
 
       return deferred.promise;
     });
-  }
-
-  buildProfileManifest(versions) {
-    let deferred = q.defer(),
-        self = this;
-
-    deferred.resolve({
-      profiles: { },
-      versions: versions
-    });
-
-    return deferred.promise;
-  }
-
-  buildManifests () {
-    return this.buildVersionManifest()
-      .then(this.buildProfileManifest);
   }
 
 };
